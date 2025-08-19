62284c0d6fb1f488b8b3c4a3efdeecd6740f0fd1
projectpaw2025 <projectpawwow@gmail.com>
2025-08-19 14:11:24 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,4 @@
 // js/firebase.js
-// 브라우저 ESM 환경에서 Firebase SDK를 CDN으로 불러옵니다.
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getFirestore, collection, addDoc, getDocs, getDoc, doc,
@@ -10,25 +8,22 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
-// 🔑 네가 준 Firebase 프로젝트 설정
 const firebaseConfig = {
   apiKey: "AIzaSyCNguz8K5MehFR5nydZ293hI60FQ9Jh5Tk",
   authDomain: "projectpaw-bf042.firebaseapp.com",
   projectId: "projectpaw-bf042",
-  storageBucket: "projectpaw-bf042.firebasestorage.app",
+  storageBucket: "projectpaw-bf042.appspot.com",   // ✅ 여기 수정됨
   messagingSenderId: "340056180297",
   appId: "1:340056180297:web:20ae730ee45b0563062198",
   measurementId: "G-FEMJ80972P"
 };
 
-// Initialize
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storageFB = getStorage(app);
 export const analytics = getAnalytics(app);
 
-// Firestore helper re-exports
 export {
   collection, addDoc, getDocs, getDoc, doc,
   query, orderBy, serverTimestamp, updateDoc
